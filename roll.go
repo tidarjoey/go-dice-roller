c591c6c8c7c453d2f30ed1cdf8010a8c41672022
Tidar <tidarharyo@rocketmail.com>
2019-01-24 04:01:44 +0700

    add console prompt

--- roll.go
+++ roll.go
@@ -4,11 +4,40 @@ import (
     "fmt"
     "math/rand"
     "time"
+    "bufio"
+    "os"
+    "strings"
 )
 
 func main() {
-    rand.Seed(time.Now().UTC().UnixNano())
-    fmt.Println(randomString(10))
+  fmt.Print(
+    `=====================================
+    Welcome to dice roller app!
+    Are you ready to roll the dice? (Y/N)
+    =====================================`
+  )
+
+  reader := bufio.NewReader(os.Stdin)
+  resp, _, err := reader.ReadRune()
+
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+
+  switch resp {
+  case "Y":
+    // call roll()
+    break
+  case "N"
+    // call okay we are ready when you are
+    break
+  }
+}
+
+func roll()  {
+  rand.Seed(time.Now().UTC().UnixNano())
+  fmt.Println(randomString(10))
 }
 
 func randomString(int) int{
